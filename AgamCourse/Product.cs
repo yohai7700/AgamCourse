01ddba7ad0ebbd0583ebc4133c707ea4f239e586
YohaiM <yohai7700@gmail.com>
2021-10-10 16:28:39 +0300

    Add Product class

---
+++ AgamCourse/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgamCourse
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
