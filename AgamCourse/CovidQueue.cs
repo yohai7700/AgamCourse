42a583544aff37a6cb1afe5f148400e3cdbe65b5
YohaiM <yohai7700@gmail.com>
2021-10-10 10:03:53 +0300

    Add CovidQueue class

---
+++ AgamCourse/CovidQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgamCourse
+{
+    class CovidQueue
+    {
+        private const float MinBodyTemperature = 30f;
+        Queue<Person> queue;
+
+        public void Enqueue(Person person)
+        {
+            ValidateEnqueue(person);
+            queue.Enqueue(person);
+        }
+
+        public Person Dequeue()
+        {
+            return queue.Dequeue();
+        }
+        private void ValidateEnqueue(Person person)
+        {
+            if (!person.Masked) throw new Exception("Can't insert an unmasked person to the Queue.");
+            if (person.Quarantined) throw new Exception("Can't insert a quarantined person to the Queue.");
+            if (person.BodyTemperature > MinBodyTemperature) throw new Exception($"Cant insert a person with a body temperature higher than {person.BodyTemperature}C");
+        }
+    }
+}
---
+++ AgamCourse/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgamCourse
+{
+    class Person
+    {
+        public float BodyTemperature { get; set; }
+        public bool Masked { get; set; }
+
+        public bool Quarantined { get; set; }
+    }
+}
