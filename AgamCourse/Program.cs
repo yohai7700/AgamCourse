a7d5c4db968811926f570dc1780887a711bf889b
Yohai <Yohai@DESKTOP-2T8AB4D>
2021-10-10 09:39:14 +0300

    Add project files.

---
+++ AgamCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgamCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
